e0b7d76a565a5469059604bdcf1da3b5314ca22d
baizhi958216 <1475289190@qq.com>
2021-01-30 16:24:18 +0800

    sm8150: Kang privapp-permissions-qti and qti_whitelist from LA.UM.9.3.r1-00500-sdm845.0

--- common.mk
+++ common.mk
@@ -83,6 +83,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
