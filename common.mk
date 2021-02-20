aceda26a838545d17089aa6bbd7e00a38f85cc55
baizhi958216 <1475289190@qq.com>
2021-02-20 23:11:25 +0800

    sm8150: overlay: Import common files * We have another sm8150 Soc devices

--- common.mk
+++ common.mk
@@ -8,7 +8,8 @@ $(call inherit-product, vendor/meizu/sm8150-common/sm8150-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
