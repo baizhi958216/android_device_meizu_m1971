1d140838f5872939d783aa8ce3fa3a6611d22941
baizhi958216 <1475289190@qq.com>
2021-02-21 11:20:02 +0800

    sm8150: Remove vndk-sp

--- common.mk
+++ common.mk
@@ -107,11 +107,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.2-service.meizu_sm8150
 
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk-sp \
-    com.android.vndk.current.on_vendor
-
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
 
 # Wifi
