59d08ad3322525930c022c92989a29f842d3ea56
baizhi958216 <1475289190@qq.com>
2021-02-22 00:08:14 +0800

    Revert "sm8150: Force enable DC."
    This reverts commit 7cb437a6ebb38cfb50df7ee087bfb2ecb9b07849.

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -40,11 +40,10 @@
 // #define NOTIFY_ENABLE_PAY_ENVIRONMENT 1609
 // #define NOTIFY_DISABLE_PAY_ENVIRONMENT 1610
 
+#define BOOST_ENABLE_PATH "/sys/class/meizu/fp/qos_set"
 #define DIMMING_SPEED_PATH "/sys/class/meizu/lcm/display/dimming_speed"
 #define HBM_ENABLE_PATH "/sys/class/meizu/lcm/display/hbm"
 #define BRIGHTNESS_PATH "/sys/class/backlight/panel0-backlight/brightness"
-#define DC_PATH "/sys/class/meizu/lcm/display/DC_Enable"
-#define DC_Level_PATH "/sys/class/meizu/lcm/display/DC_Level" 306
 
 #define TOUCHPANAL_DEV_PATH "/dev/input/" FOD_INPUT
 
@@ -147,9 +146,9 @@ Return<void> FingerprintInscreen::onPress() {
     mFingerPressed = true;
     std::thread([this]() {
         std::this_thread::sleep_for(std::chrono::milliseconds(12));
-        set(DC_PATH, 0);
         set(HBM_ENABLE_PATH, 1);
         set(DIMMING_SPEED_PATH, 1);
+        set(BOOST_ENABLE_PATH, 1);
         LOG(INFO) << "onPress: HBM is on!";
         std::this_thread::sleep_for(std::chrono::milliseconds(120));
          if (mFingerPressed) {
@@ -163,14 +162,13 @@ Return<void> FingerprintInscreen::onRelease() {
     mFingerPressed = false;
     notifyHal(NOTIFY_FINGER_UP);
     set(HBM_ENABLE_PATH, 0);
-    set(DC_PATH, 1);
     LOG(INFO) << "onPress: HBM is off!";
     return Void();
 }
 
 Return<void> FingerprintInscreen::onShowFODView() {
     mIconShown = true;
-    set(DC_PATH, 0);
+    set(BOOST_ENABLE_PATH, 1);
     set(DIMMING_SPEED_PATH, 1);
     notifyHal(NOTIFY_UI_READY);
     return Void();
